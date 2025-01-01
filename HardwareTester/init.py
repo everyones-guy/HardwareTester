126bfd020e4aa232ea7edf8a74d9324e0a716039
everyones-guy <garywill64@gmail.com>
2025-01-01 16:24:28 -0600

    initialize db added arg "app"

--- HardwareTester/init.py
+++ HardwareTester/init.py
@@ -1,7 +1,6 @@
 from HardwareTester.utils.db_utils import DatabaseManager
 
-def initialize_database():
+def initialize_database(app):
     db_manager = DatabaseManager()
     db_manager.create_tables()
     return db_manager
-
