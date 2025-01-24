1243ca1b046e6d7b54b09c263b77ae459b990e5d
everyones-guy <garywill64@gmail.com>
2025-01-23 22:42:38 -0600

    upload models name update

--- HardwareTester/models/__init__.py
+++ HardwareTester/models/__init__.py
@@ -1,10 +1,12 @@
-from .user_models import User, Role, UserSettings, Token
-from .device_models import Device, Peripheral, Controller, Emulation, Blueprint
-from .project_models import Project, Milestone
 from .configuration_models import Configuration, Settings, GlobalSettings
+from .dashboard_models import DashboardData
+from .db import db
+from .device_models import Device, Peripheral, Controller, Emulation, Blueprint, Valve, DeviceFirmwareHistory, Firmware
 from .log_models import ActivityLog, Notification
 from .metric_models import Metric
+from .project_models import Project, Milestone
 from .report_models import Report
-from .dashboard_models import DashboardData
 from .test_models import TestPlan, TestStep
-from .db import db
+from .upload_models import UploadedFile
+from .user_models import User, Role, UserSettings, Token
+
